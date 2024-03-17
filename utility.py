fe738215062a073c34bd52cd2e50f07c1b4de9db
sheepingshroom <145060676+sheepingshroom@users.noreply.github.com>
2024-03-17 20:14:20 +0100

    Delete utility.py

--- utility.py
+++
@@ -1,6 +0,0 @@
-#evaluates select options and returns matching number
-def evaluate_select(var, options : list[str], default_value = 1) -> int:
-    for i in range(len(options)):
-        if (var == options[i]):
-            return i + 1
-    return default_value
