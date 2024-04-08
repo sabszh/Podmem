11fd58a03340cd0d25cc85240f5ba92849b0ba2f
sheepingshroom <gustavidunsloth@hotmail.com>
2024-04-08 23:06:32 +0200

    local fonts

--- test.py
+++
@@ -1,6 +0,0 @@
-from youtube_transcript_api import YouTubeTranscriptApi
-from youtube_transcript_api.formatters import TextFormatter
-transcript = YouTubeTranscriptApi.get_transcript('glgU6o4nZQc', languages=["en"])
-formatter = TextFormatter()
-txt_transcript = formatter.format_transcript(transcript)
-print(txt_transcript)
