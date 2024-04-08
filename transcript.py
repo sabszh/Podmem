b209d8ab920a3fbe7ba955a55d63b56dd40e963b
sheepingshroom <gustavidunsloth@hotmail.com>
2024-04-08 18:13:40 +0200

    proxy test

--- transcript.py
+++ transcript.py
@@ -40,7 +40,7 @@ def get_video_info(id):
         raise Exception("Video doesn't exist.")
 
 def get_transcript(id):
-    transcript = YouTubeTranscriptApi.get_transcript(id, languages=["en"])
+    transcript = YouTubeTranscriptApi.get_transcript(id, languages=["en"], proxies={'https': 'proxy.server:3128'})
     formatter = TextFormatter()
     txt_transcript = formatter.format_transcript(transcript)
     return txt_transcript
