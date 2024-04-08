58e64c246708ffb050aa641520d6fbdf1ec72719
sheepingshroom <gustavidunsloth@hotmail.com>
2024-04-08 20:38:06 +0200

    test

--- test.py
+++ test.py
@@ -3,4 +3,4 @@ from youtube_transcript_api.formatters import TextFormatter
 transcript = YouTubeTranscriptApi.get_transcript('glgU6o4nZQc', languages=["en"])
 formatter = TextFormatter()
 txt_transcript = formatter.format_transcript(transcript)
-print(transcript)
+print(txt_transcript)
